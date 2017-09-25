111c62dff9b712af785587f1fc5acf044068ea84
HQ <5462117+Ropestar1@users.noreply.github.com>
2017-09-25 14:18:02 -0700

    Array reference example.

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,18 +10,19 @@ let name2 = name;
 console.log(name, name2); // output: (Wes, Wes)
 name = 'Wesley';
 console.log(name, name2); // output: (Wesley, Wes)
+// NOTE: above section of examples is different when arrays are used. With arrays (see below), if new variable assignments that point to an array are REFERENCING the array and not making a copy of the array.
 
 // Let's say we have an array
 const players = ['Wes', 'Sarah', 'Ryan', 'Poppy'];
 
-// and we want to make a copy of it.
+// and we want to make a copy of it. NOTE(FYI): this actually is an array reference, not a copy.
+const team = players;
+console.log(players, team);
 
 // You might think we can just do something like this:
-
+team[3] = 'Lux';
 // however what happens when we update that array?
-
 // now here is the problem!
-
 // oh no - we have edited the original array too!
 
 // Why? It's because that is an array reference, not an array copy. They both point to the same array!
